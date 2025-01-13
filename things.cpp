2d6093557f036fb3f2242f55c40a275dd2cf3b48
DoraTHH <erikerikssonsepost@gmail.com>
2025-01-13 09:41:49 +0100

    file

---
+++ things.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+#if !defined(THINGS_H)
+#define THINGS_H
+
+typedef class BankAccount {
+    private:
+        double balance;
+        int accountNumber;
+
+    public:
+        BankAccount (int input)
+        {
+            input = accountNumber;
+        };
+
+        int getNumber ()
+        {
+            return accountNumber;
+        };
+
+        void deposit (int input);
+
+        void withdraw (int input);
+
+        int getBalance ();
+
+} BankAccount;
+
+typedef class Bank {
+    private:
+        std::map<int, BankAccount> database;
+    
+    public: 
+        void addAccount();
+
+        void showAccountDetails();
+        
+} Bank
+
+
+#endif // THINGS_H
